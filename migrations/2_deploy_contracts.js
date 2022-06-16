3c914827457e50b8f6c31a7a414b1e7abeba9f51
Smit Padaliya <smitpadaliya5@gmail.com>
2022-06-16 20:00:15 +0530

    setup

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,15 @@
+const EthSwap = artifacts.require("EthSwap");
+const Token = artifacts.require("Token");
+
+module.exports = async function(deployer) {
+  
+  await deployer.deploy(Token);
+  const token = await Token.deployed()
+
+
+  await deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed()
+
+  await token.transfer(ethSwap.address,'1000000000000000000000000')
+
+};
